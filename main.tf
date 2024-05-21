1ac0b3c5d9b8a642ec09c946cac51fffbef2cb26
Galih Saputro <iriantogalih1@gmail.com>
2024-05-21 09:30:49 +0700

    add version 5.0 in provider aws

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
+
 provider "aws" {
+  version = "~> 5.0"
   region     = "ap-southeast-1"  
 } 
 
-resource "aws_default_vpc" "default" {
+resource "aws_default_vpc" "project1_VPC" {  
+
+  cidr_block = "12.0.0.0/16"
+
   tags = {
     Name = "main_VPC"
   }
 }
+
 
