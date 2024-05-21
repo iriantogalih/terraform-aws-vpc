a53a250ef6b32e81a4523c5938389589f3521856
Galih Saputro <iriantogalih1@gmail.com>
2024-05-21 09:38:17 +0700

    update penulisan aws_vpc dari aws_default_vpc

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 
 provider "aws" {
-  version = "~> 5.0"
   region     = "ap-southeast-1"  
 } 
 
-resource "aws_default_vpc" "project1_VPC" {  
-
-  cidr_block = "12.0.0.0/16"
+resource "aws_vpc" "project1_VPC" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "main_VPC"
